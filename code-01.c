6f768d2bfcf03d26b24b8e86d88cd63cae8dcc01
Boom rattaphon <rattaphon.i@ku.th>
2024-01-26 13:11:46 +0700

    Update Hello Git in C and Hello Git in C++

--- code-01.c
+++ code-01.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    printf("hello git");
+    char text[] ="hello git";
+    printf("%s", text);
     return 0;
 }
---
+++ code-02.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // cout : console output
+    cout << "hello git in c++" << endl;
+    return 0;
+}
